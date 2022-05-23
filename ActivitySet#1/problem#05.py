549c9158fbbd70784d3561b4e2a0cbf7fc2da027
sangameshwar hatti <100182981+Sangameshwar29hatti@users.noreply.github.com>
2022-05-23 04:29:56 +0000

    problem 5

--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -1,12 +1,15 @@
-# Functions
-
-
-def computepay(h, r):
-    pass  # ...
-
-
-hrs = float(input("Enter hours? "))
-rte = float(input("Enter rate per hour? "))
-
-p = computepay(hrs, rte)
-print("Pay", p)
+score = float(input("Enter Score: "))
+if score >=0.9:
+    printf("A")
+    
+elif score>=0.8:
+    print("B")
+elif score>=0.7:
+    printf("C")
+elif score>=0.6:
+    printf("D")
+elif score>=0.6:
+    printf("F")
+    
+else:
+    print("error")
